f21c76b89ce607b1562e562074e7a42e0b33f1e7
s3e4r <feerws22@outlook.com>
2025-03-31 03:40:30 -0700

    Add files via upload

---
+++ wqwq.js
@@ -0,0 +1,25 @@
+const baseURL = 'https://api.github.com';
+const user = '<YOUR_USERNAME>';
+
+function getToken() {
+  //change to your token to run in browser, but set
+  //back to '' before committing so all tests pass
+  return '';
+}
+
+function forkRepo() {
+  const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
+  //use fetch to fork it!
+}
+
+function showResults(json) {
+  //use this function to display the results from forking via the API
+}
+
+function createIssue() {
+  //use this function to create an issue based on the values input in index.html
+}
+
+function getIssues() {
+  //once an issue is submitted, fetch all open issues to see the issues you are creating
+}
